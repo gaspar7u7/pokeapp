e89fdccedc15395c2189a061c56c4376b837dde3
gaspar <gaspar7u7@gmail.com>
2022-12-22 03:16:34 -0600

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import pikachu from '../img/pikachu.png'
+
+const Footer = () => {
+  return (
+    <article className='footer'>
+        <footer>
+            <img src={pikachu} alt="" />
+        </footer>
+    </article>
+  )
+}
+
+export default Footer
